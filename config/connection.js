6b74614406ccdd40c60e7363b3d056760c7e11c5
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-11 16:44:53 +0100

    trying to fix error

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        user: "root",
+        password: "Sarim123",
+        database: "ecommerce_db",
+        // // employees_db
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
